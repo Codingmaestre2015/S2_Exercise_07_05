1e68d732185144a3b50e252a81595349b0869662
Patrick Clawson <patrick.clawson@west-mec.org>
2018-03-02 07:20:25 -0700

    Added more features
    Yay

--- photos.js
+++ photos.js
@@ -15,6 +15,7 @@
 /* global variables */
 var photoOrder = [1, 2, 3, 4, 5];
 var figureCount = 3;
+var autoAdvance = setInterval(rightAdvance, 1000);
 
 /* add src values to img elements based on order specified in photoOrder array */
 function populateFigures() {
@@ -35,8 +36,14 @@ function populateFigures() {
     }
 }
 
-/* shift all images one figure to the left, and change values in photoOrder array to match  */
+/* stop automatic image switching and call rightAdvance() function*/
 function rightArrow() {
+    clearInterval(autoAdvance);
+    rightAdvance();
+}
+
+/* shift all images one figure to the left, and change values in photoOrder array to match  */
+function rightAdvance() {
     for (var i = 0; i < 5; i++) {
         if ((photoOrder[i] + 1) === 6) {
             photoOrder[i] = 1;
@@ -49,6 +56,7 @@ function rightArrow() {
 
 /* shift all images one figure to the right, and change values in photoOrder array to match  */
 function leftArrow() {
+    clearInterval(autoAdvance);
     for (var i = 0; i < 5; i++) {
         if ((photoOrder[i] - 1) === 0) {
             photoOrder[i] = 5;
@@ -107,7 +115,9 @@ function previewThree() {
     var numberButton = document.querySelector("#fiveButton p");
     articleEl.removeChild(document.getElementById("fig1"));
     articleEl.removeChild(document.getElementById("fig5"));
+
     figureCount = 3;
+
     numberButton.innerHTML = "Show more images";
     numberButton.removeEventListener("click", previewThree);
     numberButton.addEventListener("click", previewFive);
@@ -115,7 +125,8 @@ function previewThree() {
 
 /* open center figure in separate window */
 function zoomFig() {
-
+    var zoomWindow = window.open("zoom.html", "zoomwin", "width=960,height=600");
+    zoomWindow.focus();
 }
 
 /* create event listeners for left arrow, right arrow, and center figure element*/
--- zoom.js
+++ zoom.js
@@ -17,7 +17,19 @@ var figFilename = "images/IMG_0" + photoOrderArray[2] + ".jpg";
 
 /* populate img element and create event listener */
 function pageSetup() {
-   document.getElementsByTagName("img")[0].src = figFilename; // assign filename to img element
+    document.getElementsByTagName("img")[0].src = figFilename; // assign filename to img element
+    createEventListener();
+}
+
+/* close window*/
+function closeWin() {
+    window.close();
+}
+
+/* create an event listener for the close button*/
+function createEventListener() {
+    var closeWindowDiv = document.getElementsByTagName("p")[0];
+    closeWindowDiv.addEventListener("click", closeWin);
 }
 
 /* add img src value and create event listener when page finishes loading */
